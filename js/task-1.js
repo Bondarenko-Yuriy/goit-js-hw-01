c27b05360956129e98900d2558cf1e8953d2aa20
Yuriy <provokator1986@gmail.com>
2024-02-21 20:39:46 +0200

    Fin

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@ function makeTransaction(quantity, pricePerDroid) {
   //   console.log(
   //     'You ordered ' + quantity + ' droids worth ' + totalPrice + ' credits!'
   //   );
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
--- js/task-3.js
+++ js/task-3.js
@@ -3,8 +3,8 @@ function getElementWidth(content, padding, border) {
   let Widthpadding = parseFloat(padding);
   let Widthborder = parseFloat(border);
 
-  console.log(Widthcontent + Widthpadding * 2 + Widthborder * 2);
+  return Widthcontent + Widthpadding * 2 + Widthborder * 2;
 }
-getElementWidth('50px', '8px', '4px');
-getElementWidth('60px', '12px', '8.5px');
-getElementWidth('200px', '0px', '0px');
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
